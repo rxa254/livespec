bd276038004bbd7e445726f65e77b357052309bf
Rana X <rana@caltech.edu>
2023-03-24 15:14:52 -0700

    fixin GPT code

--- GPTspec.py
+++ GPTspec.py
@@ -2,11 +2,9 @@ import pyaudio
 import numpy as np
 import matplotlib.pyplot as plt
 
-p = pyaudio.PyAudio()
-
 # Parameters for the spectrogram
 NFFT = 1024  # Number of points for the FFT
-Fs = 44100   # Sampling frequency
+Fs = 8192   # Sampling frequency
 
 # Initialize the plot
 fig, ax = plt.subplots()
@@ -16,29 +14,29 @@ img = ax.imshow(np.zeros((NFFT // 2 + 1, 100)), extent=[0, 1, 0, Fs/2], cmap='in
 def update_specgram(in_data, frame_count, time_info, status):
     # Compute the spectrogram
     spec = np.abs(np.fft.rfft(np.frombuffer(in_data, dtype=np.int16), n=NFFT))
-    spec = 20 * np.log10(spec)
-    
+    spec = (spec / np.max(spec))
+
     # Update the plot data
     img.set_data(np.hstack([img.get_array()[:, 1:], np.atleast_2d(spec).T]))
 
-    #img.setImage(img_array, autoLevels=True)
     # Redraw the plot
     fig.canvas.draw()
+    return (in_data, pyaudio.paContinue)
 
 # Start the microphone input stream and update the spectrogram
-
+p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=Fs, input=True, frames_per_buffer=NFFT, stream_callback=update_specgram)
 
 stream.start_stream()
 
 # Continuous update loop
-plt.show(block=False)
 while stream.is_active():
-    plt.pause(0.01)
+    plt.pause(0.2)
 
 # Stop the stream and close the audio device
 stream.stop_stream()
 stream.close()
 p.terminate()
+
 
 
